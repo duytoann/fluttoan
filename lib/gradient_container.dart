cf960b9c066cdb07c10a702c54c6890d68c4e854
Nguyen Duy Toan <duytoan.ng@samsung.com>
2023-12-22 08:34:10 +0700

    impl styling buttons, images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,27 +1,55 @@
-import 'package:FlutCombine/styled_text.dart';
 import 'package:flutter/material.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  void rollDice() {}
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 8),
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText("Hola Duy Toan"),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                  // padding: const EdgeInsets.only(
+                  //   top: 20,
+                  // ),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                  )),
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
       ),
     );
   }
